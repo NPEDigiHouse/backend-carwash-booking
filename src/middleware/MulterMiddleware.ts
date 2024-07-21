3c995b909333be8aaabfafa580d12102a415a24a
rafmasloman <raflymasloman20@gmail.com>
2024-07-21 19:30:47 +0800

    feat: add multer middleware

--- src/middleware/MulterMiddleware.ts
+++ src/middleware/MulterMiddleware.ts
@@ -1,12 +1,16 @@
 import multer from 'multer';
 
-const diskStorage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, 'public/uploads/');
-  },
-  filename(req, file, callback) {
-    callback(null, file.originalname);
-  },
-});
+const diskStorage = (folderName: string) => {
+  return multer.diskStorage({
+    destination(req, file, callback) {
+      callback(null, `public/uploads/${folderName}`);
+    },
+    filename(req, file, callback) {
+      callback(null, file.originalname);
+    },
+  });
+};
 
-export const upload = multer({ storage: diskStorage });
+export const upload = (folderName: string) => {
+  return multer({ storage: diskStorage(folderName) });
+};
